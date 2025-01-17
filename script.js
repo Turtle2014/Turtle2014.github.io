96c9f4b1b8e759b2edb738a640b655bb9eadc345
Turtle2014 <jfrancis88219@my.npsct.org>
2025-01-17 17:48:43 -0500

    Advanced
    You kids wouldn't understand.Even I don't

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ var audio = new Audio("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
 
 audio.loop = true
 audio.play()
+document.getElementById("nana").addEventListener("click", function() {window.location.href = "https://www.youtube.com/playables/UgkxytGBPWPaTj-4F9cRGT8X1JB-j_mbFsN4"});
