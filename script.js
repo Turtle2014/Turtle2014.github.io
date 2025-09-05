7641dd9b5bf0a317951ed5a4ec7bd2aeba869de7
Turtle2014 <jfrancis88219@my.npsct.org>
2025-09-05 17:36:55 -0400

    All of Elons Info IS MINE

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ var audio = new Audio("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
 audio.loop = true
 audio.play()
 document.getElementById("nana").addEventListener("click", function() {window.location.href = "https://www.youtube.com/playables/UgkxytGBPWPaTj-4F9cRGT8X1JB-j_mbFsN4"});
+
+/*Username:ElonMusk
+Password:Money$
+Gmail:elonmusk@gmail.com
+Website:Look in the omni bar
+Phone Number: 1(803)566-6875
+Birthdays:June 28, 1971, 11:58 PM
+he does not own a masion or a yacht But he owns a Tesla.*/ 
