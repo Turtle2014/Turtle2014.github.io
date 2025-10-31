883f6fa7b30398aa435ffa5980fb6ae64153073b
Turtle2014 <jfrancis88219@my.npsct.org>
2025-10-31 17:18:06 -0400

    Add login function for user authentication AGIAANNANANANANANAN
    BOOOOOORRIING

--- script.js
+++ script.js
@@ -22,3 +22,13 @@ form.addEventListener('submit', function (e) {
 const user = document.getElementById('username').value;
 const pass = document.getElementById('password').value;
 
+function login() {
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+
+  if (user === "ElonMusk" && pass === "Money$") {
+    alert("Access Granted ✅");
+  } else {
+    alert("Locked Out ❌");
+  }
+}
