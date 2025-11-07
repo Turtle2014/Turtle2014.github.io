2c48c98bd9feacaf8d8a9381755e9a6b1666ef05
Turtle2014 <jfrancis88219@my.npsct.org>
2025-11-07 17:41:12 -0500

    Add audio playback and click event for redirect

--- script2.js
+++ script2.js
@@ -1,3 +1,14 @@
+var audio = new Audio("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHh.mp3");
+
+audio.loop = true
+audio.play()
+document.getElementById("nana").addEventListener("click", function() {window.location.href = "https://www.youtube.com/playables/UgkxytGBPWPaTj-4F9cRGT8X1JB-j_mbFsN4"});
+
+/*Username:ElonMusk
+Password:Money$
+Gmail:elonmusk@gmail.com
+Birthdays:June 28, 1971*/
+
 function login() {
   const user = document.getElementById("username").value;
   const pass = document.getElementById("password").value;
